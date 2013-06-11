b33435049088d9af2a34772fa753f5640cdafb6b
customer-backend <customer-backend@cloud.heahdk.net>
2013-06-11 19:06:12 +0200

    input checking

--- server/modules/customer.js
+++ server/modules/customer.js
@@ -9,32 +9,53 @@ exports.requestCaptcha = function(cb) {
   captcha.get(cb);
 };
 
+function validEmail(str) {
+  return typeof(str)=='string' && str.match(/^([a-z0-9\.\_\+\%\-]+@[a-z0-9\.\-]+\.[a-z]+)$/g);
+                                               //  (chars)           @ (chars)      .(letters)
+}
+
+function validPassword(str) {
+  return typeof(str)=='string' && str.length > 5;
+  //any string of length >= 5
+}
+
+function validHost(str) {
+  return typeof(str)=='string' && str.match(/^([a-z0-9][a-z0-9\-]*[a-z0-9])$/g);
+                                             // (one lc) (lc or -)  (one lc)
+}
+
 exports.requestAccount = function(agree, email, pwd, captchaToken, captchaSolution, host, cb) {
-  if(agree) {
- 
-   if(captcha.resolve(captchaToken, captchaSolution)) {
-      customer.createAccount(email, pwd, function(err, uid) {
-        if(err) {
-          cb(err);
-        } else {
-          var serverId = 0;
-          rs.createRemotestorage(uid, serverId, host, 5000, function(err2) {
-            if(err2) {
-              cb(err2);
-            } else {
-              var root = config.serverRoot[serverId]+host+'/public/dns/whois/'+host+'.un.ht/';
-              domain.createDomain(host, uid, root+'admin/', root+'tech/', root+'ns/', function(err3) {
-                session.create(uid, cb);
-             });
-            }
-          });
-        }
-      });
-    } else {
-      cb('captcha wrong');
-    }
-  } else {
+  email = email.toLowerCase().trim();
+  host = host.toLowerCase().trim();
+  if(agree != 'Be nice!') {
     cb('please agree to be nice');
+  } else if(!captcha.resolve(captchaToken, captchaSolution)) {
+    cb('captcha wrong');
+  } else if(!validEmail(email)) {
+    cb('email not valid');
+  } else if(!validPassword(pwd)) {
+    cb('password not valid');
+  } else if(!validHost(host)) {
+    cb('host not valid');
+  } else {
+    customer.createAccount(email, pwd, function(err, uid) {
+      if(err) {
+        cb(err);
+      } else {
+        var serverId = 0;
+        rs.createRemotestorage(uid, serverId, host, 5000, function(err2) {
+          if(err2) {
+            cb(err2);
+          } else {
+            var root = config.serverRoot[serverId]+host+'/public/dns/whois/'+host+'.un.ht/';
+            domain.createDomain(host, uid, root+'admin/', root+'tech/', root+'ns/', function(err3) {
+    	        session.create(uid, cb);
+            });
+        
+          }
+        });
+      }
+    });
   }
 };
 
