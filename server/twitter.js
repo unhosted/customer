dd97126644df5d3a3f702e12a746a283da803859
customer-backend@unht-beta <customer-backend@unht-beta>
2013-06-21 19:20:55 +0200

    create dirs

--- server/twitter.js
+++ server/twitter.js
@@ -8,36 +8,41 @@ exports.retrieve = function(keys, cb) {
   //  console.log(err, reply);
   //});
 
-  //fs.mkdirSync('storage/');
-  //fs.mkdirSync('storage/microblog/');
-  //fs.mkdirSync('storage/microblog/microposts/');
-  //fs.mkdirSync('storage/profile/');
-  //fs.mkdirSync('storage/www/');
-  twit.get('statuses/user_timeline', function(err, tweets) {
-    var profileSaved = false;
-    //console.log(err, tweets);
-    for(var i = 0; i<tweets.length; i++) {
-      //console.log(tweets[i].text);
-      //console.log(tweets[i].created_at);
-      fs.writeFile('storage/microblog/microposts/'+uuid(), JSON.stringify({
-        text: tweets[i].text,
-        created_at: tweets[i].created_at
-      }), function(err) {
-        if(err) {
-          console.log(err);
-        }
+  fs.mkdir('storage/', function(err) {
+    fs.mkdir('storage/microblog/', function(err2) {
+      fs.mkdir('storage/microblog/microposts/', function(err3) {
+        fs.mkdir('storage/profile/', function(err4) {
+          fs.mkdir('storage/www/', function(err5) {
+            twit.get('statuses/user_timeline', function(err, tweets) {
+              var profileSaved = false;
+              //console.log(err, tweets);
+              for(var i = 0; i<tweets.length; i++) {
+                //console.log(tweets[i].text);
+                //console.log(tweets[i].created_at);
+                fs.writeFile('storage/microblog/microposts/'+uuid(), JSON.stringify({
+                  text: tweets[i].text,
+                  created_at: tweets[i].created_at
+                }), function(err) {
+                  if(err) {
+                    console.log(err);
+                  }
+                });
+                //console.log(tweets[i].user.screen_name);
+                //console.log(tweets[i].user.name);
+                //console.log(tweets[i].user.profile_image_url);
+                //console.log(tweets[i].user.url);
+                //console.log(tweets[i].user.description);
+                //console.log(tweets[i].user.location);
+                if(!profileSaved) {
+                  saveProfile(tweets[i].user, cb);
+                  profileSaved = true;
+                }
+              }
+            });
+          });
+        });
       });
-      //console.log(tweets[i].user.screen_name);
-      //console.log(tweets[i].user.name);
-      //console.log(tweets[i].user.profile_image_url);
-      //console.log(tweets[i].user.url);
-      //console.log(tweets[i].user.description);
-      //console.log(tweets[i].user.location);
-      if(!profileSaved) {
-        saveProfile(tweets[i].user, cb);
-        profileSaved = true;
-      }
-    }
+    });
   });
 };
 function saveProfile(user, cb) {
